e42ce8fc7796da6027506c1daf18e2b9e784a135
Diana PF <149829535+KitsuneDai@users.noreply.github.com>
2024-04-28 22:58:14 +0000

    Cambio color texto footer y quitado icons

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,26 +1,26 @@
 import React, { Component } from "react";
 import "../../styles/footer.css";
 import MediConecta from "../../img/MediConecta.jpg"
-import "./../../styles/footer.css"
+ 
 
 export const Footer = () => {
 
-  const handleInstagram = () => {
-    // Redirige al componente Login
-    navigate('Instagram');
-  };
-  const handleFacebook = () => {
-    // Redirige al componente Login
-    navigate('Facebook');
-  };
-  const handleTwitter= () => {
-    // Redirige al componente Login
-    navigate('Twitter');
-  };
-  const handleLinkedin = () => {
-    // Redirige al componente Login
-    navigate('Linkedin');
-  };
+  // const handleInstagram = () => {
+  //   // Redirige al componente Login
+  //   navigate('Instagram');
+  // };
+  // const handleFacebook = () => {
+  //   // Redirige al componente Login
+  //   navigate('Facebook');
+  // };
+  // const handleTwitter= () => {
+  //   // Redirige al componente Login
+  //   navigate('Twitter');
+  // };
+  // const handleLinkedin = () => {
+  //   // Redirige al componente Login
+  //   navigate('Linkedin');
+  // };
 
   return(
 	<footer className="footer mt-auto py-3 text-center">
@@ -34,12 +34,11 @@ export const Footer = () => {
               <h2 className="ftco-heading-2">Especialidades</h2>
               <div className="">
                 <ul className="list-unstyled">
-                <li>
-                  
+                {/* <li>
                   <button className="btn btn-Facebook" style={{color: "white"}}onClick={handleFacebook}>
                   <i className="fa-brands fa-facebook fa-xl"></i>
                   </button>
-              </li>
+                </li> */}
                   <li><a href="#"><span className="icon-long-arrow-right mr-2"></span>Medicina General</a></li>
                   <li><a href="#"><span className="icon-long-arrow-right mr-2"></span>Pediatría</a></li>
                   <li><a href="#"><span className="icon-long-arrow-right mr-2"></span>Dermatología</a></li>
@@ -56,10 +55,10 @@ export const Footer = () => {
 
               <ul className="list-unstyled">
                 <li><a href="#home-section">Inicio</a></li>
-                <li><a href="#about-section">Sobre Nosotros</a></li>
+                {/* <li><a href="#about-section">Sobre Nosotros</a></li> */}
                 <li><a href="#department-section">Departamentos</a></li>
                 <li><a href="#doctor-section">Doctores</a></li>
-                <li><a href="#blog-section">Blog</a></li>
+                {/* <li><a href="#blog-section">Blog</a></li> */}
                 <li><a href="#">Precios</a></li>
                 <li><a href="#contact-section">Contactenos</a></li>
               </ul>
@@ -69,10 +68,10 @@ export const Footer = () => {
             <div className="ftco-footer-widget mb-4">
 
               <div className="">
-                  <a href="/"><img src={MediConecta} alt="MediConecta" /></a>
-              <p className="mt-2">Muy muy lejos, detrás de la palabra montañas, lejos de los países..</p>
+                  <a href="/"><img src={MediConecta} alt="MediConecta" style={{borderRadius:"50%"}}/></a>
+              {/* <p className="mt-2">Muy muy lejos, detrás de la palabra montañas, lejos de los países..</p> */}
               </div>
-              <div className="d-flex justify-content-center">
+              {/* <div className="d-flex justify-content-center">
               <div className="Instagram">
                   <button className="btn btn-Instagram" style={{color: "white"}}onClick={handleInstagram}>
                   <i className="fab fa-instagram fa-xl"></i> 
@@ -93,7 +92,7 @@ export const Footer = () => {
                   <i className="fa-brands fa-linkedin fa-xl"></i>
                   </button>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="col-md">
@@ -101,8 +100,8 @@ export const Footer = () => {
               <h2 className="ftco-heading-2">Servicios</h2>
 
               <ul className="list-unstyled">
-                <li> <span className="icon-long-arrow-right mr-2">Lunes a domingo</span> </li>
-                <li> <span className="icon-long-arrow-right mr-2">24h</span></li>
+                <li> <span className="icon-long-arrow-right mr-2" style={{color:"#ffff"}}>Lunes a domingo</span> </li>
+                <li> <span className="icon-long-arrow-right mr-2" style={{color:"#ffff"}}>24h</span></li>
               </ul>
             </div>
           </div>
@@ -111,20 +110,20 @@ export const Footer = () => {
               <h2 className="ftco-heading-2">Contacto</h2>
               <div className="block-23 mb-3">
                 <ul className="list-unstyled">
-                  <li><span className="icon icon-map-marker"></span><span className="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                  <li><a href="#"><span className="icon icon-phone"></span><span className="text">+32 569 874 875</span></a></li>
-                  <li><a href="#"><span className="icon icon-envelope pr-4"></span><span className="text">info@mediconecta.com</span></a></li>
+                  <li><span className="icon icon-map-marker"></span><span className="text" style={{color:"#ffff"}}>203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                  <li><a href="#"><span className="icon icon-phone"></span><span className="text" style={{color:"#ffff"}}>+32 569 874 875</span></a></li>
+                  <li><a href="#"><span className="icon icon-envelope pr-4"></span><span className="text" style={{color:"#ffff"}}>info@mediconecta.com</span></a></li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
+      </div>
+      <div className="row" style={{ backgroundColor: "#000000", color: "#ffff", width: "100%", margin:"0", paddingTop:"15px"}}>
           <div className="col-md-12 text-center">
             <p>Todos los derechos reservados | Web diseñada por Diana Pérez - José Reimondez - Ruben MP para <a href="https://4geeksacademy.com/es/inicio" target="_blank" rel="noopener noreferrer">4geek</a> © Copyright 2024</p>
           </div>
         </div>
-      </div>
     </footer>
   
   )
