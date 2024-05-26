7ab6a457c401a0005d667c7514edec58783c7245
Diana PF <149829535+KitsuneDai@users.noreply.github.com>
2024-05-26 02:08:29 +0000

    Perfilando detalles de contraseña y readme

--- README.md
+++ README.md
@@ -8,25 +8,25 @@ Bootstrap, Cloudinary, CSS, Figma, HTML, JavaScript, Phyton, React.js, sqlAlchem
 
 ## 🏥 Sobre el proyecto
 - **HOME**
-  - En donde encontramos un resumen de la web en general, pudiendo ver las especialidades que tenemos, los médicos asistenciales que son parte de manera global del equipo médico. Y el contacto.
+  -Aquí encontramos un resumen de la web en general, pudiendo ver y acceder a las especialidades que tenemos, parte de los médicos asistenciales del equipo médico y  el contacto.
   
 - **CONTACTO**
   - Hay una opción para contactar con el soporte de dicha web por si hay dudas, quejas, sugerencias por parte de los usuarios del servicio.
   
 - **ESPECIALIDADES**
-  - En este apartado podemos entrar un breve resumen de los servicios que se pueden esperar de una consulta online, y en cada sección podemos acceder a los médicos que practican la misma y a su vez, acceder a la información más detallada de los mismos.
+  - En este apartado podemos encontrar un breve resumen de los servicios que se pueden esperar de una consulta online, en cada sección podemos acceder a los médicos que practican la misma y a su vez, acceder a la información más detallada de dichos médicos.
   
 - **REGISTRO Y AUTENTIFICACIÓN**
-   - Registro de usuario ya sea paciente o médico y proporciona información personal, tras el registro, recibe un email de bienvenida.
-   - Y ya puede iniciar sesión para acceder a su perfil.
-   - También si el usuario se olvida su contraseña puede solicitar un reseteo de la misma. Todo esto lo explicará con más detalle mis compañeros.
+   - Registro de usuario ya sea paciente o médico y proporcionar información personal, tras el registro, recibe un email de bienvenida.
+   - Si el usuario se olvida de la contraseña puede solicitar un reseteo de la misma, recibiendo un email con una contraseña aleatoria.
+   - Una vez logueado puede cambiar la contraseña de nuevo.
    
 - **GESTIÓN DE CITAS/CONSULTA ONLINE**
-   - Los pacientes registrados podrán acceder a pedir cita con el médico de su elección y recibirán un correo de recordatorio con el enlace para acceder a su consulta.
+   - Los pacientes registrados podrán acceder para pedir cita con el médico de su elección y recibirán un correo recordatorio con el enlace para acceder a su consulta.
    
 - **MANEJO DE DOCUMENTOS**
    - Los usuarios pacientes pueden cargar documentos médicos relevantes a través de un formulario desde su perfil.
-   - El acceso a dichos documentos está también habilitado para el médico para que en el caso que lo necesite, pueda leerlos antes de la consulta.
+   - El acceso a dichos documentos está también habilitado para el médico en el caso que lo necesite, pueda leerlos antes de la consulta.
    
 - **SEGURIDAD**
    - No se puede acceder a los perfiles de otros sin la autentificación necesaria.
--- src/front/js/component/CambioContraseña.js
+++ src/front/js/component/CambioContraseña.js
@@ -11,19 +11,37 @@ export const CambioContraseña = () => {
     const [success, setSuccess] = useState(false);
     const [submitted, setSubmitted] = useState(false); // Variable para rastrear si se ha enviado el formulario
     
+    const isValidPassword = (password) => {
+        const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$#@!])[A-Za-z\d$#@!]{6,12}$/;
+        return pattern.test(password);
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setSubmitted(true); // Se marca como enviado el formulario
         setError(""); // Limpiar el mensaje de error al enviar el formulario
         setSuccess(false); // Limpiar el mensaje de éxito al enviar el formulario
+
         if (password !== confirmPassword) {
             setError("Las contraseñas no coinciden");
+        } else if (!isValidPassword(password)) {
+            setError(
+                <div>
+                    <p>La contraseña no cumple con los siguientes requisitos de seguridad:</p>
+                    <ul style={{ fontSize: '0.9em' }}>
+                        <li>Al menos 1 letra entre [a-z]</li>
+                        <li>Al menos 1 número entre [0-9]</li>
+                        <li>Al menos 1 letra entre [A-Z]</li>
+                        <li>Al menos 1 carácter de [$#@!]</li>
+                        <li>Longitud mínima de la contraseña: 6</li>
+                        <li>Longitud máxima de la contraseña: 12</li>
+                    </ul>
+                </div>
+            );
         } else {
             actions.changePassword(password, id)
                 .then(() => {
                     setSuccess(true);
-                    // setPassword("");
-                    // setConfirmPassword("");
                     setTimeout(() => {
                         setSubmitted(false); // Restablecer submitted después de 3 segundos
                         navigate(-1);
@@ -34,44 +52,47 @@ export const CambioContraseña = () => {
                 });
         }
     };
+
     const handleInputChange = () => {
         setError(""); // Limpiar el mensaje de error al editar los campos de contraseña
         setSuccess(false); // Limpiar el mensaje de éxito al editar los campos de contraseña
     };
+
     const handleGoBack = () => {
         setError(""); // Limpiar el mensaje de error al volver
         navigate(-1);
     };
+
     return (
         <div className="container" style={{ marginTop: "30px" }}>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label>Password:</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    value={password}
-                    onChange={(e) => { setPassword(e.target.value); handleInputChange(); }}
-                />
-            </div>
-            <div className="form-group">
-                <label>Confirma Password:</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    value={confirmPassword}
-                    onChange={(e) => { setConfirmPassword(e.target.value); handleInputChange(); }}
-                />
-            </div>
-            {/* Validación del campo requerido solo si el formulario ha sido enviado */}
-            {submitted && (password === "" || confirmPassword === "") && <p style={{ color: "red" }}>Debe completar todos los campos</p>}
-            {/* {error && <p style={{ color: "red" }}>{error}</p>} */}
-            {success && <p style={{ color: "green" }}>¡Contraseña cambiada exitosamente!</p>}
-            <div className="d-flex justify-content-around">
-                <button type="submit" className="btn btn-primario">Actualizar</button>
-                <button className="btn btn-secondary" onClick={handleGoBack}>Volver</button>
-            </div>
-        </form>
-    </div>
-);
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        value={password}
+                        onChange={(e) => { setPassword(e.target.value); handleInputChange(); }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Confirma Password:</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        value={confirmPassword}
+                        onChange={(e) => { setConfirmPassword(e.target.value); handleInputChange(); }}
+                    />
+                </div>
+                {/* Validación del campo requerido solo si el formulario ha sido enviado */}
+                {submitted && (password === "" || confirmPassword === "") && <p style={{ color: "red" }}>Debe completar todos los campos</p>}
+                {error && <p style={{ color: "red" }}>{error}</p>}
+                {success && <p style={{ color: "green" }}>¡Contraseña cambiada exitosamente!</p>}
+                <div className="d-flex justify-content-around">
+                    <button type="submit" className="btn btn-primario">Actualizar</button>
+                    <button className="btn btn-secondary" onClick={handleGoBack}>Volver</button>
+                </div>
+            </form>
+        </div>
+    );
 };
